3061b2739b339e9d06b1f113971cbf835acfcea7
bananaappletw <dudarevadi22@yandex.ru>
2024-06-10 19:29:37 +0300

    test commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ global FUZZ, file_name, args, output_file, dict_name
 
 FUZZ = "qW3r7y_A5d_4sD_1234567890" 
 g = copy.deepcopy(FUZZ)
-args = ['weasrdtfgyhjkihyugtrfyguhygtrfgyutfff']
-file_name = 'Test_examples/trees'
+args = ['1','qW3r7y_A5d_4sD_1234567890','1']
+file_name = 'Test_examples/calc'
 output_file = 'output.txt'
 dict_name = 'dict.txt'
